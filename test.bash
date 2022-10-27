130f491e862eaed10b2ac28d20a905f38b509e78
Soma <s21C1093JG@s.chibakoudai.jp>
2022-10-27 12:36:15 +0900

    Add test2

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Souma Nomoto
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
