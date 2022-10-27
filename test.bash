d2c0a0b57ede362349bd3f028b7b38d9a163e9bd
Soma <s21C1093JG@s.chibakoudai.jp>
2022-10-27 13:02:46 +0900

    delete nomoto

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2022 Souma Nomoto
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -11,7 +11,16 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15.0 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
